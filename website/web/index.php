a659b78c8e44c067cfc3ece29c26134d3cb1fcc9
yanni11 <yannitsar@gmail.com>
2017-03-09 10:25:17 +0100

    Add new Demoroute

--- website/web/index.php
+++ website/web/index.php
@@ -6,16 +6,14 @@ require_once("../vendor/autoload.php");
 $tmpl = new ihrname\SimpleTemplateEngine(__DIR__ . "/../templates/");
 
 switch($_SERVER["REQUEST_URI"]) {
+	
+	case "/testroute":
+		echo "Test bla bla";
+		break;
 	case "/":
 		(new ihrname\Controller\IndexController($tmpl))->homepage();
 		break;
-	case "/test/upload":
-		if(file_put_contents(__DIR__ . "/../../upload/test.txt", "Mein erster Upload")) {
-			echo "It worked";
-		} else {
-			echo "Error happened";
-		}
-		break;
+	
 	default:
 		$matches = [];
 		if(preg_match("|^/hello/(.+)$|", $_SERVER["REQUEST_URI"], $matches)) {
